b458891e4ee86ce5ba95586f86c301262d7a535d
Christian R. Guarin <cguarin@blaine.local>
2020-09-14 09:32:04 +0800

    sample 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                        <h2>Hello world 1</h2>
                     {{ __('You are logged in!') }}
                 </div>
             </div>
