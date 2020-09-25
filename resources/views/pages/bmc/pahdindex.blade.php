38e0d9806796092d6dea9b0e1496c1328bb8b385
Jesa Nina C. Lacambra <jlacambra@blaine.local>
2020-09-25 13:20:18 +0800

    parallax image rev

--- resources/views/pages/bmc/pahdindex.blade.php
+++ resources/views/pages/bmc/pahdindex.blade.php
@@ -9,7 +9,7 @@
           </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="/images/SampleCarousel/Picture3.jpg" alt="First slide">
+                <img class="d-block w-100" src="/images/Sample/LabItem.jpg" alt="First slide">
                 <div class="carousel-caption">
                     <h2 class="animated fadeInLeft">Taste the world with us</h2>
                     <p class="animated fadeInUp">Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
@@ -18,10 +18,10 @@
                 </div>
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="/images/SampleCarousel/Picture2.jpg" alt="Second slide">
+                <img class="d-block w-100" src="/images/Sample/LiquidSoap.jpg" alt="Second slide">
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="/images/SampleCarousel/Picture1.jpg" alt="Third slide">
+                <img class="d-block w-100" src="/images/Sample/BarSoap.jpg" alt="Third slide">
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
@@ -37,48 +37,52 @@
     <div id="parallax-world-of-ugg">
       
         <section>
+            <div class="block">
+                <h2 class="bmc-pc2">Blaine Personal and Home Care Corporation</h2>
+                <p>BPHCC has come a long way in terms of improving its processes 
+                  through equipment upgrade and personnel training. It tied up 
+                  with partners known locally and abroad to design state-of-the-art 
+                  machinery for its manufacturing plant. Its competent team of 
+                  young and talented individuals are pouring their efforts into 
+                  R&D activities and in tapping suppliers of the finest raw 
+                  materials across the globe.</p>
+                <p class="margin-top-10" style="font-style: italic"><b>All of these are driven to one goal of
+                    bringing quality products and value for money to clients.</b> 
+                </p>
+            </div>
+        </section>
+
+        <section>
             <div class="parallax-one">
-              <h2 style="text-align: left">SOUTHERN CALIFORNIA</h2>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus odit, esse aperiam pariatur modi accusamus vel recusandae obcaecati libero nulla corrupti eveniet quibusdam quae officia eum, corporis incidunt veritatis laboriosam!</p>
+                <div class="col-md-6" class="bmc-pc3">
+                    <h3 style="text-align: left; padding-left:15px" class="text-opacity">Blaine intensifies expansion to respond to the growing demand of food industry.</h3>
+                    <p style="text-align: left; color:black; padding-left:15px">Blaine Corporation established its personal and home care division in 2009 to 
+                            cater to the basic cleaning care needs of its employees, manufacturing facilities and existing clients. 
+                            The division expanded into a toller, trader and manufacturer of personal and home care products of 
+                            various reputable companies.<br><br>
+                            In 2018, Blaine Manufacturing Corporation was formed. Resources and capabilities of Blaine Food Corporation 
+                            and Blaine Personal and Home Care Corporation were consolidated to respond to the growing market demand.
+                    </p>
+                </div>
             </div>
         </section>
         
         <section>
-          <div class="block">
-            <p>BPHCC has come a long way in terms of improving its processes through equipment upgrade and personnel training. It tied up with partners known locally and abroad to design state-of-the-art machinery for its manufacturing plant. Its competent team of young and talented individuals are pouring their efforts into R&D activities and in tapping suppliers of the finest raw materials across the globe.</p>
-            <p class="line-break margin-top-10"></p>
-            <p class="margin-top-10">The UGG brand began to symbolize those who embraced sport and a relaxed, active lifestyle. More than that, an emotional connection and a true feeling of love began to grow for UGG boots, just as Brian had envisioned. People didn't just like wearing UGG boots, they fell in love with them and literally could not take them off. By the end of the 90's, celebrities and those in the fashion world took notice of the UGG brand. A cultural shift occurred as well - people were embracing, and feeling empowered, by living a more casual lifestyle and UGG became one of the symbols of this lifestyle. By 2000, a love that began on the beaches had become an icon of casual style. It was at this time that the love for UGG grew in the east, over the Rockies and in Chicago. In 2000, UGG Sheepskin boots were first featured on Oprah's Favorite Things® and Oprah emphatically declared that she "LOOOOOVES her UGG boots." From that point on, the world began to notice.</p>
-          </div>
+            <div class="parallax-two">
+                <div class="col-md-6" style="float: right">
+                    <h2 style="text-align: left">Blaine Manufacturing Corporation is committed to safety and quality.</h2>
+                    <p style="text-align:left">Blaine complies and upholds the highest standards of quality and safety management. 
+                    The company is certified by local and international regulatory body:
+                    <br><br>
+                    • License to Operate (LTO) as Cosmetic Manufacturer
+                    <br>
+                    • License to Operate (LTO) as Drug Manufacturer
+                    <br>
+                    • FDA certified for Good Manufacturing Practices
+                    <br>
+                    • Certified for Halal Plant Accreditation</p>
+                </div>
+            </div>
         </section>
-        
-        <section>
-          <div class="parallax-two">
-            <h2 style="text-align: right">NEW YORK</h2>
-            <p style="text-align:right">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-          </div>
-        </section>
-        
-        <section>
-          <div class="block">
-            <p><span class="first-character ny">B</span>reaking into the New York fashion world is no easy task. But by the early 2000's, UGG Australia began to take it by storm. The evolution of UGG from a brand that made sheepskin boots, slippers, clogs and sandals for an active, outdoor lifestyle to a brand that was now being touted as a symbol of a stylish, casual and luxurious lifestyle was swift. Much of this was due to a brand repositioning effort that transformed UGG into a high-end luxury footwear maker. As a fashion brand, UGG advertisements now graced the pages of Vogue Magazine as well as other fashion books. In the mid 2000's, the desire for premium casual fashion was popping up all over the world and UGG was now perfectly aligned with this movement.</p>
-            <p class="line-break margin-top-10"></p>
-            <p class="margin-top-10">Fueled by celebrities from coast to coast wearing UGG boots and slippers on their downtime, an entirely new era of fashion was carved out. As a result, the desire and love for UGG increased as people wanted to go deeper into this relaxed UGG experience. UGG began offering numerous color and style variations on their sheepskin boots and slippers. Cold weather boots for women and men and leather casuals were added with great success. What started as a niche item, UGG sheepskin boots were now a must-have staple in everyone's wardrobe. More UGG collections followed, showcasing everything from knit boots to sneakers to wedges, all the while maintaining that luxurious feel UGG is known for all over the world. UGG products were now seen on runways and in fashion shoots from coast to coast. Before long, the love spread even further.</p>
-          </div>
-        </section>
-        
-        <section>
-          <div class="parallax-three">
-            <h2>ENCHANTED FOREST</h2>
-          </div>
-        </section>
-        
-        <section>
-          <div class="block">
-            <p><span class="first-character atw">W</span>hen the New York fashion community notices your brand, the world soon follows. The widespread love for UGG extended to Europe in the mid-2000's along with the stylish casual movement and demand for premium casual fashion. UGG boots and shoes were now seen walking the streets of London, Paris and Amsterdam with regularity. To meet the rising demand from new fans, UGG opened flagship stores in the UK and an additional location in Moscow. As the love spread farther East, concept stores were opened in Beijing, Shanghai and Tokyo. UGG Australia is now an international brand that is loved by all. This love is a result of a magical combination of the amazing functional benefits of sheepskin and the heightened emotional feeling you get when you slip them on your feet. In short, you just feel better all over when you wear UGG boots, slippers, and shoes.</p>
-            <p class="line-break margin-top-10"></p>
-            <p class="margin-top-10">In 2011, UGG will go back to its roots and focus on bringing the active men that brought the brand to life back with new styles allowing them to love the brand again as well. Partnering with Super Bowl champion and NFL MVP Tom Brady, UGG will invite even more men to feel the love the rest of the world knows so well. UGG will also step into the world of high fashion with UGG Collection. The UGG Collection fuses the timeless craft of Italian shoemaking with the reliable magic of sheepskin, bringing the luxurious feel of UGG to high end fashion. As the love for UGG continues to spread across the world, we have continued to offer new and unexpected ways to experience the brand. The UGG journey continues on and the love for UGG continues to spread.</p>
-          </div>
-        </section>
-          
         </div>  
 @endsection    
