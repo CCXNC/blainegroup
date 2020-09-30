a48c5c19f6c910c2d648368648b1572f556031cc
Jesa Nina C. Lacambra <jlacambra@blaine.local>
2020-09-30 13:23:04 +0800

    technical services to recheck

--- resources/views/pages/rrlc/technicalservice.blade.php
+++ resources/views/pages/rrlc/technicalservice.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.rrlcapp')
 
 @section('content')
-    <img class="d-block w-100" src="/images/123.png" alt="First slide">
+    <img class="d-block w-100" src="/images/rrlc/recheck/recheck_logo.png" alt="First slide">
     <br>
     <section class="page-title">
         <div class="container">
@@ -15,11 +15,24 @@
             <div class="tech-1-wrapper">
                 <div class="card">
                     <div class="row">
-                        <div class="col-md-4">
-                            <img src="/images/blank.png" width="200px;">
+                        <div class="col-md-12">
+                            <b>Farm Audit and Troubleshooting</b>
                         </div>
-                        <div class="col-md-8">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi atque aut vitae magnam similique. Ipsa dolorem soluta id sint aut molestiae harum quos inventore necessitatibus commodi ullam hic, non dolorum.
+                        <div class="col-md-4">
+                            <img src="/images/rrlc/recheck/services.jpg" width="200px;">
+                        </div>
+                        <div class=" col-md-8">
+                            <p> 
+                                <b>Feed Formulation and Animal Health</b><br>
+                                <b>Program Assistance</b><br>
+                                Our technical team can assist the farm in feed formulation design and assessment.
+                                We also give preventive and therapeutic program based on farm audit activities.
+                                <br><br>
+                                <b>Precise Nutrition Evaluation</b><br>
+                                A raw material nutrient analysis through NIR program of Adisseo that give
+                                comprehensive and precise digestible nutrient valued (CP, ME amino acids etc.)
+                                That is useful for raw material classification, feed formulation design and
+                                animal performance assessment.
                                 <a href="#" class="pull-right">READ MORE</a>
                             </p>
                         </div>
@@ -27,11 +40,20 @@
                 </div>
                 <div class="card">
                     <div class="row">
+                        <div class="col-md-12">
+                            <b>Farm Training</b>
+                        </div>
                         <div class="col-md-4">
-                            <img src="/images/blank.png" width="200px;">
+                            <img src="/images/rrlc/recheck/training.jpg" width="200px;">
                         </div>
                         <div class="col-md-8">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi atque aut vitae magnam similique. Ipsa dolorem soluta id sint aut molestiae harum quos inventore necessitatibus commodi ullam hic, non dolorum.
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi atque aut 
+                                vitae magnam similique. Ipsa dolorem soluta id sint aut molestiae harum 
+                                quos inventore necessitatibus commodi ullam hic, non dolorum. Lorem 
+                                ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia 
+                                minus perferendis alias natus animi ab ratione sed labore tenetur! 
+                                Itaque quaerat a exercitationem libero voluptate consequatur distinctio 
+                                doloremque veniam.
                                 <a href="#" class="pull-right">READ MORE</a>
                             </p>
                         </div>
@@ -39,11 +61,19 @@
                 </div>
                 <div class="card">
                     <div class="row">
+                        <div class="col-md-12">
+                            <b>Mycotoxin Management 
+                                <br> Program</b>
+                        </div>
                         <div class="col-md-4">
-                            <img src="/images/blank.png" width="200px;">
+                            <img src="/images/rrlc/recheck/mycoscan.jpg" width="200px;">
                         </div>
                         <div class="col-md-8">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eveniet assumenda similique cupiditate, quasi neque? Natus recusandae aut quia cupiditate, consectetur voluptas consequuntur expedita dignissimos laboriosam veniam deleniti ullam aliquid?
+                            <p>Mycoscan is use for analysis and quantification of mycotoxin in raw materials
+                                and finished feeds for proper dosage recommendation of mycotoxin binder.
+                                <br><br>
+                               
+                                
                                 <a href="#" class="pull-right">READ MORE</a>
                             </p>
                         </div>
@@ -51,11 +81,17 @@
                 </div>
                 <div class="card">
                     <div class="row">
+                        <div class="col-md-12">
+                            <b>In-Feed Liquid Spray System
+                                <br> Calibration and Maintenance Service</b>
+                        </div>
                         <div class="col-md-4">
-                            <img src="/images/blank.png" width="200px;">
+                            <img src="/images/rrlc/recheck/RHODIMETAT88.png" width="200px;">
                         </div>
                         <div class="col-md-8">
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut velit expedita nesciunt quis nam ipsam cum, sunt aspernatur esse consequuntur ipsum. Perspiciatis harum veniam recusandae saepe fugit porro eligendi mollitia.
+                            <p>In partnership with Adisseo we provide engineering service for installation and 
+                                regular calibration and maintenance for accuracy and cost savings of Rhodimet 
+                                feed application.
                                 <a href="#" class="pull-right">READ MORE</a>
                             </p>
                         </div>
